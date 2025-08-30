d3aefe2a58d5d3b553ac2e1677a354fc28555cb3
Henry Alejandro Cáceres <henry.caceres@tecnohalecatez.com.co>
2025-08-30 12:03:05 -0500

    Update GET_ALL_USERS constant to follow naming conventions

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/tecnohalecatez/api/constant/UserConstant.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/tecnohalecatez/api/constant/UserConstant.java
@@ -1,7 +1,5 @@
 package co.com.tecnohalecatez.api.constant;
 
-import org.springframework.http.HttpStatus;
-
 public class UserConstant {
 
     public static final String BASE_PATH = "/api/v1/users";
@@ -11,7 +9,7 @@ public class UserConstant {
     public static final String GET_USER_ID = "GetUserById";
     public static final String UPDATE_USER = "UpdateUser";
     public static final String DELETE_USER_ID = "DeleteUserById";
-    public static final String GET_ALL_USERS = "getAllUsers";
+    public static final String GET_ALL_USERS = "GetAllUsers";
     public static final String INVALID_USER_DATA = "Invalid User Data Supplied";
     public static final String INTERNAL_SERVER_ERROR = "Internal Server Error";
 
