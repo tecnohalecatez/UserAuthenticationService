4e1dc253f46ed746d9d699561f78428b2d2f1821
Henry Alejandro Cáceres <henry.caceres@tecnohalecatez.com.co>
2025-09-01 22:17:57 -0500

    Refactor UserRouterRestTest to improve user handling and validation logic

--- infrastructure/entry-points/reactive-web/src/test/java/co/com/tecnohalecatez/api/UserRouterRestTest.java
+++ infrastructure/entry-points/reactive-web/src/test/java/co/com/tecnohalecatez/api/UserRouterRestTest.java
@@ -1,12 +1,13 @@
 package co.com.tecnohalecatez.api;
 
 import co.com.tecnohalecatez.api.config.UserPath;
+import co.com.tecnohalecatez.api.dto.UserDTO;
 import co.com.tecnohalecatez.api.dto.UserDataDTO;
 import co.com.tecnohalecatez.api.mapper.UserDTOMapper;
 import co.com.tecnohalecatez.model.user.User;
 import co.com.tecnohalecatez.usecase.user.UserUseCase;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
@@ -14,7 +15,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -24,6 +24,9 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doNothing;
+
+import org.springframework.validation.Validator;
 
 @ContextConfiguration(classes = {UserRouterRest.class, UserHandler.class})
 @EnableConfigurationProperties(UserPath.class)
@@ -40,12 +43,12 @@ class UserRouterRestTest {
     private UserUseCase userUseCase;
 
     @MockitoBean
-    private UserHandler userHandler;
-
-    @MockitoBean
     private UserDTOMapper userDTOMapper;
 
-    private final User testUser = User.builder()
+    @MockitoBean
+    private Validator validator;
+
+    private final User testUserOne = User.builder()
             .id(BigInteger.ONE)
             .name("John")
             .surname("Doe")
@@ -54,6 +57,17 @@ class UserRouterRestTest {
             .phone("555-1234")
             .email("john.doe@example.com")
             .baseSalary(50000.0)
+            .build();
+
+    private final User testUserTwo = User.builder()
+            .id(BigInteger.TWO)
+            .name("Johns")
+            .surname("Does")
+            .birthDate(LocalDate.of(1990, 1, 1))
+            .address("123 Main Sts")
+            .phone("555-12345")
+            .email("john.does@example.com")
+            .baseSalary(150000.0)
             .build();
 
     private final UserDataDTO testUserDataDTO = new UserDataDTO(
@@ -71,14 +85,17 @@ class UserRouterRestTest {
     @Test
     void shouldLoadUserPathProperties() {
         assertEquals(users, userPath.getUsers());
-        assertEquals("/api/v1/users/{id}", userPath.getUsersById());
+        assertEquals(users + "/{id}", userPath.getUsersById());
     }
 
     @Test
     void listenSaveUserReturnsCreated() {
-        when(userUseCase.saveUser(any(User.class))).thenReturn(Mono.just(testUser));
-        Mockito.when(userHandler.listenSaveUser(Mockito.any()))
-                .thenReturn(Mono.just(ServerResponse.created(null).build().block()));
+        UserDTO expectedUserDTO = new UserDTO(BigInteger.ONE, "John", "Doe", LocalDate.of(1990, 1, 1), "123 Main St", "555-1234", "john.doe@example.com", 50000.0);
+        doNothing().when(validator).validate(any(), any());
+        when(userUseCase.existsByEmail("john.doe@example.com")).thenReturn(Mono.just(false));
+        when(userDTOMapper.toModel(testUserDataDTO)).thenReturn(testUserOne);
+        when(userUseCase.saveUser(any(User.class))).thenReturn(Mono.just(testUserOne));
+        when(userDTOMapper.toResponse(testUserOne)).thenReturn(expectedUserDTO);
         webTestClient.post()
                 .uri(users)
                 .contentType(MediaType.APPLICATION_JSON)
@@ -89,22 +106,22 @@ class UserRouterRestTest {
 
     @Test
     void listenGetUserByIdReturnsUser() {
-        when(userUseCase.getUserById(BigInteger.ONE)).thenReturn(Mono.just(testUser));
-        Mockito.when(userHandler.listenGetUserById(Mockito.any()))
-                .thenReturn(Mono.just(ServerResponse.ok().build().block()));
-
+        UserDTO expectedUserDTO = new UserDTO(BigInteger.ONE, "John", "Doe", LocalDate.of(1990, 1, 1), "123 Main St", "555-1234", "john.doe@example.com", 50000.0);
+        when(userUseCase.getUserById(BigInteger.ONE)).thenReturn(Mono.just(testUserOne));
+        when(userDTOMapper.toResponse(testUserOne)).thenReturn(expectedUserDTO);
         webTestClient.get()
-                .uri("/api/v1/users/1")
+                .uri(users + "/1")
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
-                .expectStatus().isOk();
+                .expectStatus().isOk()
+                .expectBody(UserDTO.class)
+                .value(userDTO ->
+                        Assertions.assertThat(userDTO.id()).isEqualTo(BigInteger.ONE));
     }
 
     @Test
     void listenGetUserByIdReturnsNotFound() {
-        Mockito.when(userHandler.listenGetUserById(Mockito.any()))
-                .thenReturn(Mono.just(ServerResponse.notFound().build().block()));
-
+        when(userUseCase.getUserById(BigInteger.valueOf(999))).thenReturn(Mono.empty());
         webTestClient.get()
                 .uri("/api/v1/users/999")
                 .accept(MediaType.APPLICATION_JSON)
@@ -115,8 +132,6 @@ class UserRouterRestTest {
     @Test
     void listenDeleteUserByIdReturnsNoContent() {
         when(userUseCase.deleteUserById(BigInteger.ONE)).thenReturn(Mono.empty());
-        Mockito.when(userHandler.listenDeleteUserById(Mockito.any()))
-                .thenReturn(Mono.just(ServerResponse.noContent().build().block()));
         webTestClient.delete()
                 .uri("/api/v1/users/1")
                 .exchange()
@@ -125,15 +140,23 @@ class UserRouterRestTest {
 
     @Test
     void listenGetAllUsersReturnsAllUsers() {
-        when(userUseCase.findAllUsers()).thenReturn(Flux.just(testUser));
-        Mockito.when(userHandler.listenGetAllUsers(Mockito.any()))
-                .thenReturn(Mono.just(ServerResponse.ok().build().block()));
-
+        UserDTO userDTO1 = new UserDTO(BigInteger.ONE, "John", "Doe", LocalDate.of(1990, 1, 1), "123 Main St", "555-1234", "john.doe@example.com", 50000.0);
+        UserDTO userDTO2 = new UserDTO(BigInteger.TWO, "Johns", "Does", LocalDate.of(1990, 1, 1), "123 Main Sts", "555-12345", "john.does@example.com", 150000.0);
+        when(userUseCase.findAllUsers()).thenReturn(Flux.just(testUserOne, testUserTwo));
+        when(userDTOMapper.toResponse(testUserOne)).thenReturn(userDTO1);
+        when(userDTOMapper.toResponse(testUserTwo)).thenReturn(userDTO2);
         webTestClient.get()
                 .uri(users)
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
-                .expectStatus().isOk();
+                .expectStatus().isOk()
+                .expectBodyList(UserDTO.class)
+                .hasSize(2)
+                .value(userList -> {
+                    Assertions.assertThat(userList).isNotEmpty();
+                    Assertions.assertThat(userList.getFirst().id()).isEqualTo(BigInteger.ONE);
+                })
+        ;
     }
 
 }
