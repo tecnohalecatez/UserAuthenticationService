7526165f8f81c2aecb1a85af06c3de2bfa117474
Henry Alejandro Cáceres <henry.caceres@tecnohalecatez.com.co>
2025-09-03 18:18:11 -0500

    Implement login functionality with JWT token generation and update routing configuration

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/tecnohalecatez/api/config/LoginPath.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/tecnohalecatez/api/config/LoginPath.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Getter
 @Setter
-@ConfigurationProperties(prefix = "routes.path")
+@ConfigurationProperties(prefix = "routes.login")
 public class LoginPath {
     private String login;
 }
---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/tecnohalecatez/api/constant/LoginConstant.java
@@ -0,0 +1,7 @@
+package co.com.tecnohalecatez.api.constant;
+
+public class LoginConstant {
+    public static final String BASE_PATH = "/api/v1/Login";
+    public static final String LISTEN = "listen";
+    public static final String GET_TOKEN = "GetToken";
+}
